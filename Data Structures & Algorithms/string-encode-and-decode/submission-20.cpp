bee2ce808516ee6d3d269a6b2f947c5e5816f512
Ignacio Garro <40432800+Ignacio-Garro@users.noreply.github.com>
2026-09-08 20:54:50 +0200

    Add: string-encode-and-decode - submission-20

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-20.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string sol;
+        for (string s:strs) {
+            string prefix = to_string(s.size()) + "#";
+            sol += prefix;
+            sol += s;
+        }
+
+        return sol;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> sol;
+        string result;
+        string word;
+        std::stringstream ss(s);
+       
+        while(std::getline(ss, result, '#')){
+            word.resize(std::stoi(result));
+            ss.read(&word[0],std::stoi(result));
+            sol.push_back(word);
+        }
+        return sol;
+    }
+};
