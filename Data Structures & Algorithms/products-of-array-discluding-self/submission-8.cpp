ae3a9676ee8addc1d8a7a11900541de911ec2bd5
Ignacio Garro <40432800+Ignacio-Garro@users.noreply.github.com>
2026-09-09 15:25:32 +0200

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> sol;
+        int zeros = 0;
+        int multi = 1;
+        for(int num : nums){
+            if(num != 0){
+                multi *= num;   
+            }
+            else{
+                zeros++;
+            }
+        }
+        for(int num: nums){
+            if(zeros > 1) sol.push_back(0);
+            else if(zeros == 1){
+                if(num == 0) sol.push_back(multi);
+                else sol.push_back(0);
+            }
+            else sol.push_back(multi/num);
+        }
+        return sol;
+    }
+};
